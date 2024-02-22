07159018d6790a42509386aefb319d18fec7d37f
Chloe Bright <chloebright@Chloes-MacBook-Pro.local>
2024-02-21 20:21:32 -0500

    Added a main function that allows the user to execute all available features of the to do list.

--- todo_list.py
+++ todo_list.py
@@ -8,6 +8,33 @@ Created on Wed Feb 21 16:38:41 2024
 
 def main():
     myTasks= {}
+    cont = 'y'
+    while cont == 'y':
+        if len(myTasks)==0: 
+            print("Welcome to your to-do list. It is currently empty. Enter a task to begin.")
+            myTasks = addTask(myTasks)
+            print("Here is your current to-do list: ", viewTasks(myTasks))
+            cont = input("Would you like to continue? Enter y for yes, n for no:   ")
+        else:
+            choice = input(""" Enter the appropriate character for the action you would like to complete:
+    a to add a task
+    d to delete a task
+    v to view your tasks
+    c to mark a task as complete:\n
+                       """)
+            if choice == 'a':
+                addTask(myTasks)
+                print("Here is your updated to-do list: ", viewTasks(myTasks))
+            elif choice == 'd':
+                deleteTask(myTasks)
+                print("Here is your updated to-do list: ", viewTasks(myTasks))
+            elif choice == 'v':
+                print("Here is your current to-do list: ", viewTasks(myTasks))
+            elif choice == 'c':
+                markComplete(myTasks)
+                print("Here is your updated to-do list: ", viewTasks(myTasks))
+            cont = input("Would you like to continue? Enter y for yes, n for no:   ")
+    print("Thank you for using your to-do list. Here is your final list of tasks: \n", viewTasks(myTasks))
     
         
     
